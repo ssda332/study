206bf2276b041c778ab92b930130c8c190933d9a
ssda332 <tititiqkfk3@gmail.com>
2024-04-29 23:27:21 +0900

    string

---
+++ src/com/backjoon/string/BOJ1120_문자열.java
@@ -0,0 +1,31 @@
+package com.backjoon.string;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1120_문자열 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= B.length() - A.length(); i++) {
+            String sub = B.substring(i, i + A.length());
+            int result = 0;
+
+            for (int j = 0; j < sub.length(); j++) {
+                if (A.charAt(j) != sub.charAt(j)) result++;
+            }
+
+            if (answer > result) answer = result;
+        }
+
+//        dfs(A);
+
+        System.out.println(answer);
+    }
+
+}
